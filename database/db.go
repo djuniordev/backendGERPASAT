d6213ca69c5213bd893a2415496af6686085c283
djuniordev <danieljunior.me@gmail.com>
2024-11-16 11:43:13 -0300

    dasdasd teste

--- database/db.go
+++ database/db.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,9 +14,30 @@ var (
 )
 
 func ConectaComBancoDeDados() {
+	// String de conexão com o Supabase
 	stringDeConexao := "host=aws-0-sa-east-1.pooler.supabase.com user=postgres.jkhvplmlxwsxrwycupwf password=0lTaIsIsuPU2Y0gZ dbname=postgres port=6543"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+
+	// Abrir a conexão com o banco de dados
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{
+		// Configurações adicionais, se necessário
+	})
+
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Fatalf("Erro ao conectar com banco de dados: %v", err)
 	}
+
+	// Configuração do pool de conexões
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Erro ao obter o DB do GORM: %v", err)
+	}
+
+	// Definindo o número máximo de conexões abertas
+	sqlDB.SetMaxOpenConns(10) // Número máximo de conexões abertas com o banco
+	// Definindo o número máximo de conexões ociosas
+	sqlDB.SetMaxIdleConns(5) // Número de conexões ociosas
+	// Definindo o tempo máximo de vida das conexões
+	sqlDB.SetConnMaxLifetime(30 * time.Minute) // Tempo máximo de vida de cada conexão
+
+	log.Println("Conectado ao banco de dados com sucesso!")
 }
